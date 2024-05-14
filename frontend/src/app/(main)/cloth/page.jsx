953e8d6aef40c1d067d48c18e28170abfbbf29f3
iharshitsahu <harshitmtr705@gmail.com>
2024-05-14 13:09:32 +0530

    yes

--- frontend/src/app/(main)/cloth/page.jsx
+++ frontend/src/app/(main)/cloth/page.jsx
@@ -1,24 +1,28 @@
-'use client';
-import  { useEffect, useState } from "react";
+"use client";
+import { useEffect, useState } from "react";
 import { useParams } from "next/navigation";
 import { IoSearch } from "react-icons/io5";
-
+import {
+  IconFilter,
+  IconMicrophone,
+  IconShoppingCart,
+} from "@tabler/icons-react";
 import toast from "react-hot-toast";
 import useCartContext from "@/context/CartContext";
-
+import Link from "next/link";
 
 const Showpiece = () => {
-  
   const [productList, setProductList] = useState([]);
-  const {isInCart,addItemToCart} = useCartContext();
+  const { isInCart, addItemToCart } = useCartContext();
 
   const [product, setProduct] = useState([]);
-
 
   const { category } = useParams();
 
   const fetchShowpieceproduct = async () => {
-    const res = await fetch("http://localhost:5000/product/getbycategory/cloth");
+    const res = await fetch(
+      "http://localhost:5000/product/getbycategory/cloth"
+    );
 
     console.log(res.status);
 
@@ -37,104 +41,131 @@ const Showpiece = () => {
 
   const displayproductData = () => {
     if (product.length === 0) {
-      return <h1 className='text-center fw-bold' style={{ color: "seagreen" }}>No Data Found</h1>
+      return (
+        <h1 className="text-center fw-bold" style={{ color: "seagreen" }}>
+          No Data Found
+        </h1>
+      );
     }
 
     return product.map((product) => (
       <>
-        <div className='grid grid-cols-3 mt-4 mx-12 '>
-          <div className=" bg-transparent " style={{ border: "none" }}>
-            <img src={'http://localhost:5000/' + product.images} alt="" className="card-img-top img-fluid" style={{ objectFit: "cover", height: 250 }} />
-           
-            <div className="card-footer" style={{ border: "none", height: "200px" }}>
-              <h2 className=' fw-semibold fs-5 mt-3 mb-3' style={{ fontFamily: "serif" }}>{product.pname}</h2>
-              <p className='text-red-700' style={{ fontFamily: "cursive" }}>{product.pprice}</p>
-
-              <p className='text-black  mb-3' style={{ fontFamily: "cursive" }}>{product.pcategory}</p>
-              <button disabled={isInCart(product)} onClick={e => addItemToCart(product)} className='mt-2 mb-2 bg-green-600 hover:bg-green-700 py-1 px-3 text-white mx-auto  block rounded' >
-            {isInCart(product) ? 'Already Added' : 'Add to Cart'}
-          </button>
-
+        <div className="">
+          <div className="relative m-10  flex w-full max-w-xs flex-col overflow-hidden rounded-lg border border-gray-100 bg-purple-50 shadow-md">
+            <Link
+              className="relative mx-3 mt-3 flex h-60 overflow-hidden  rounded-xl"
+              href={"/productDetail/" + product._id}
+            >
+              <img
+                className="object-contain block m-auto h-60 w-full"
+                src={`${process.env.NEXT_PUBLIC_API_URL}/${product.images[0]}`}
+                alt={product.pname}
+              />
+            </Link>
+            <div className="mt-4 px-5 pb-5">
+              <a href="#">
+                <h5 className="text-xl tracking-tight text-slate-900">
+                  {product.pname}
+                </h5>
+              </a>
+              <p className="mt-5">
+                Sold by : {product.seller.fname} {product.seller.lname}
+              </p>
+              <div className="mt-2 mb-5 flex items-center justify-between">
+                <p>
+                  <span className="text-3xl font-bold text-slate-900">
+                    ₹{product.pprice}
+                  </span>
+                  {/* <span className="text-sm text-slate-900 line-through">$699</span> */}
+                </p>
+                <div className="flex items-center">
+                  <svg
+                    aria-hidden="true"
+                    className="h-5 w-5 text-yellow-300"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+                  </svg>
+                  <svg
+                    aria-hidden="true"
+                    className="h-5 w-5 text-yellow-300"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+                  </svg>
+                  <svg
+                    aria-hidden="true"
+                    className="h-5 w-5 text-yellow-300"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+                  </svg>
+                  <svg
+                    aria-hidden="true"
+                    className="h-5 w-5 text-yellow-300"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+                  </svg>
+                  <svg
+                    aria-hidden="true"
+                    className="h-5 w-5 text-yellow-300"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+                  </svg>
+                  <span className="mr-2 ml-3 rounded bg-yellow-200 px-2.5 py-0.5 text-xs font-semibold">
+                    5.0
+                  </span>
+                </div>
+              </div>
+              <button
+                disabled={isInCart(product)}
+                onClick={(e) => addItemToCart(product)}
+                className="my-2 bg-green-600 hover:bg-green-700 py-2 px-5 text-white block w-full rounded"
+              >
+                <IconShoppingCart className="inline" />{" "}
+                {isInCart(product) ? "Already Added" : "Add to Cart"}
+              </button>
             </div>
           </div>
         </div>
       </>
-    ))
-
+    ));
   };
 
-
   const filterProducts = (e) => {
-      const value = e.target.value;
-      setProductList(productList.filter((cat) => {
-          return cat.pname.toLowerCase().includes(value.toLowerCase()),
+    const value = e.target.value;
+    setProductList(
+      productList.filter((cat) => {
+        return (
+          cat.pname.toLowerCase().includes(value.toLowerCase()),
           cat.category.toLowerCase().includes(value.toLowerCase())
+        );
       })
-      );
+    );
   };
 
   return (
     <div className="pt-16 ">
+      
 
-<div
-  className="relative flex"
-  data-twe-input-wrapper-init=""
-  data-twe-input-group-ref=""
->
-  <input
-    type="search"
-    className="peer   min-h-[auto] w-1/2  rounded border-black bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary data-[twe-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-white dark:placeholder:text-neutral-300 dark:autofill:shadow-autofill dark:peer-focus:text-primary [&:not([data-twe-input-placeholder-active])]:placeholder:opacity-0"
-    placeholder="Search"
-    aria-label="Search"
-    onChange={filterProducts}
-    id="exampleFormControlInput"
-    aria-describedby="basic-addon1"
-  />
-  <label
-    htmlFor="exampleFormControlInput"
-    className="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[twe-input-state-active]:-translate-y-[0.9rem] peer-data-[twe-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-400 dark:peer-focus:text-primary"
-  >
-    Search
-  </label>
-  <button
-    className="relative z-[2] -ms-0.5 flex items-center rounded-e bg-primary px-5  text-xs font-medium uppercase leading-normal text-white shadow-primary-3 transition duration-150 ease-in-out hover:bg-primary-accent-300 hover:shadow-primary-2 focus:bg-primary-accent-300 focus:shadow-primary-2 focus:outline-none focus:ring-0 active:bg-primary-600 active:shadow-primary-2 dark:shadow-black/30 dark:hover:shadow-dark-strong dark:focus:shadow-dark-strong dark:active:shadow-dark-strong"
-    type="button"
-    id="button-addon1"
-    data-twe-ripple-init=""
-    data-twe-ripple-color="light"
-  >
-    <span className="[&>svg]:h-5 [&>svg]:w-5">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        strokeWidth="1.5"
-        stroke="currentColor"
-      >
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
-        />
-      </svg>
-    </span>
-  </button>
-</div>
-
-
-    <div className="container-fluid ">
-      <div className="container">
-        <div className="row  ">
-          {displayproductData()}
+      <div className="container-fluid ">
+        <div className="container">
+          <div className="p-2  md:p-0 grid grid-cols-1  sm:grid-cols-2 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-3 gap-10 items-start  ">{displayproductData()}</div>
         </div>
       </div>
     </div>
-
-
-
-
-
-  </div>
   );
 };
 
