d991a28ecccaefa4af2282dd25c0ac9430aa2ca4
iharshitsahu <harshitmtr705@gmail.com>
2024-05-14 13:34:56 +0530

    ji

--- frontend/src/app/user/checkout/page.jsx
+++ frontend/src/app/user/checkout/page.jsx
@@ -203,32 +203,7 @@ const CheckOut = () => {
                   </div>
                 </label>
               </div>
-              <div className="relative">
-                <input
-                  className="peer hidden"
-                  id="radio_2"
-                  type="radio"
-                  name="radio"
-                  defaultChecked=""
-                />
-                <span className="peer-checked:border-gray-700 absolute right-4 top-1/2 box-content block h-3 w-3 -translate-y-1/2 rounded-full border-8 border-gray-300 bg-white" />
-                <label
-                  className="peer-checked:border-2 peer-checked:border-gray-700 peer-checked:bg-gray-50 flex cursor-pointer select-none rounded-lg border border-gray-300 p-4"
-                  htmlFor="radio_2"
-                >
-                  <img
-                    className="w-14 object-contain"
-                    src="/images/oG8xsl3xsOkwkMsrLGKM4.png"
-                    alt=""
-                  />
-                  <div className="ml-5">
-                    <span className="mt-2 font-semibold">Fedex Delivery</span>
-                    <p className="text-slate-500 text-sm leading-6">
-                      Delivery: 2-4 Days
-                    </p>
-                  </div>
-                </label>
-              </div>
+              
             </form>
           </div>
           <div className="mt-10 bg-gray-50 px-4 pt-8 lg:mt-0">
